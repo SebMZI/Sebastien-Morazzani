ae7ff1a0c354a5f76e5575cf61be3a925eee39f0
SebMZI <49833807+SebMZI@users.noreply.github.com>
2024-01-04 19:36:14 +0100

    feat: add sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ export const metadata: Metadata = {
   title: "Augmenter votre visibilité | Sébastien Morazzani-Marigny",
   description:
     "Élevez votre entreprise vers de nouveaux sommets en investissant dans sa visibilité : Le pouvoir de la croissance par l'investissement stratégique. Investir dans son entrerprise, c'est avant tout avoir le pouvoir de lui donner de la visibilité.",
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
