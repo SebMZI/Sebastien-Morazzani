36c4e93ae362a211d631341e284cc80dad068614
SebMZI <49833807+SebMZI@users.noreply.github.com>
2024-01-17 23:28:49 +0100

    feat: modified webmanifest

--- next.config.js
+++ next.config.js
@@ -3,8 +3,5 @@ const withPWA = require("next-pwa");
 module.exports = withPWA({
   pwa: {
     dest: "public",
-    register: true,
-    skipWaiting: true,
   },
-  // Add any other Next.js configuration here if needed
 });
