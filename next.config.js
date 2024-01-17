0edd10e7b8483a32496271ac9e55f9f0e3244ae4
SebMZI <49833807+SebMZI@users.noreply.github.com>
2024-01-17 23:30:52 +0100

    feat: modified webmanifest

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-const withPWA = require("next-pwa");
-
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-  },
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
 });
+
+const nextConfig = withPWA({
+  // next config
+});
+module.exports = nextConfig;
